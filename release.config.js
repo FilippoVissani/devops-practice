df3241e6acbebc86706448b032207bb43e647ae4
Filippo Vissani <filippo.vissani@studio.unibo.it>
2022-12-15 12:14:56 +0100

    ci: Fixed Semantic Release configuration

--- release.config.js
+++ release.config.js
@@ -3,7 +3,7 @@ var publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 2
 git push --force origin \${nextRelease.version} || exit 3
 `
-var config = require('semantic-release-preconfigured-conventional-commits');
+var config = require('semantic-release-preconfigured-conventional-commits')
 config.plugins.push(
     ["@semantic-release/exec", {
         "publishCmd": publishCmd,
